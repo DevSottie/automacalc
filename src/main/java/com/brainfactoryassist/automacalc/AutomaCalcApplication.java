0d661954da50affff40a482de0e0b9e407b571e5
João Vitor <40897957+DevSottie@users.noreply.github.com>
2023-08-01 19:47:26 -0300

    Cria projeto

---
+++ src/main/java/com/brainfactoryassist/automacalc/AutomaCalcApplication.java
@@ -0,0 +1,13 @@
+package com.brainfactoryassist.automacalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brainfactoryassist/automacalc/AutomaCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brainfactoryassist.automacalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
